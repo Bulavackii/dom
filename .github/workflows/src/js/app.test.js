41d90c040db046b6d551845d4863f8302dcc9952
Denis <bulavatsky@list.ru>
2024-02-05 00:34:50 +0300

    add web.yml

--- .github/workflows/src/js/app.test.js
+++ .github/workflows/src/js/app.test.js
@@ -1,9 +1,1 @@
-describe("Пример теста", () => {
-  test.each([
-    { str: "Hello!", expected: "Demo: Hello!" },
-    { str: "", expected: "Demo: " },
-    { str: 100, expected: "Demo: 100" },
-  ])("demo($str)", ({ str, expected }) => {
-    expect(demo(str)).toBe(expected);
-  });
-});
+// tests
